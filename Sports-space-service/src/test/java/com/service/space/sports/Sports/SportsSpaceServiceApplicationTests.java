6699aaf95ad383bbca980125984ea35555cd337c
gabrielseiko <gabriel-s98@hotmail.com>
2024-09-27 22:36:45 -0500

    Estructuras de microservices

---
+++ Sports-space-service/src/test/java/com/service/space/sports/Sports/SportsSpaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.space.sports.Sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsSpaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
